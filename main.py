77b8469cd5d2c02a4fd1bf312828bcddfa10f415
Thiago <thi.nepo@gmail.com>
2024-07-29 19:44:37 -0300

    removido os comentários.

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
+# Binary Search Algorithm
 def binary_search(arr, target):
     left, right = 0, len(arr) - 1
 
     while left <= right:
         mid = (left + right) // 2
 
-        # Check if target is present at mid
         if arr[mid] == target:
             return mid
-        # If target is greater, ignore the left half
         elif arr[mid] < target:
             left = mid + 1
-        # If target is smaller, ignore the right half
         else:
             right = mid - 1
 
-    # Target was not found in the array
     return -1
 
 
